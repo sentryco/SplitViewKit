17541540ab95d26ef5ff67198babe2809cf1a651
eonist <30n1st@gmail.com>
2024-10-20 06:52:52 +0200

    Minor tweak

--- Sources/SplitViewKit/SplitViewContainer.swift
+++ Sources/SplitViewKit/SplitViewContainer.swift
@@ -69,6 +69,7 @@ public struct SplitViewContainer<SideBar: View, Content: View, Detail: View>: Vi
       self.detail = detail
    }
 }
+
 // 🏀 Continue here
 // fix the index duplication mess 
 // - Fixme: ⚠️️ Maybe also add a sheet and alert to test them in split, portrait, compact window etc 👈 when we press detail items etc
@@ -79,4 +80,6 @@ public struct SplitViewContainer<SideBar: View, Content: View, Detail: View>: Vi
 // - Fixme: ⚠️️ Test to see if macOS still works okay 👈
 // - Fixme: ⚠️️ Add support for dark / lightmode (make different b/w gifs etc) 👈
 // - Fixme: ⚠️️ Add GA actions for building. and badge for org readme etc. and add gifs for all readmes etc
+// improve doc 
+// add emojies to problem / solution
 // - Fixme: ⚠️️ move debugContainer into example project scope

60c1609eb82745e1a9356e2dd3f1fe3e72548dbe
eonist <30n1st@gmail.com>
2024-11-19 05:37:59 +0100

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -4,7 +4,8 @@ import PackageDescription
 let package = Package(
     name: "SplitViewKit",
     platforms: [
-        .iOS(.v17), .macOS(.v14)
+         .iOS(.v17),
+         .macOS(.v14)
     ],
     products: [
         .library(
